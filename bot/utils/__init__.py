5ce66c8993d194ce657e9268f4049635d56abd28
MarkKoz <KozlovMark@gmail.com>
2020-05-08 17:32:34 -0700

    Create a utility function to count lines in a string

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -7,3 +7,11 @@ class CogABCMeta(CogMeta, ABCMeta):
     """Metaclass for ABCs meant to be implemented as Cogs."""
 
     pass
+
+
+def has_lines(string: str, count: int) -> bool:
+    """Return True if `string` has at least `count` lines."""
+    split = string.split("\n", count - 1)
+
+    # Make sure the last part isn't empty, which would happen if there was a final newline.
+    return split[-1] and len(split) == count
